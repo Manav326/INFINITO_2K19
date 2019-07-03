2cd7a277b6cdb90d9129e5ff02c95e0b51352ed2
Manav <manaviitp123@gmail.com>
2019-07-03 23:16:45 +0530

    Google LogIn is working seamlessly.

--- app/src/main/java/com/mind/INFINITO/MainActivity.java
+++ app/src/main/java/com/mind/INFINITO/MainActivity.java
@@ -3,6 +3,7 @@ package com.mind.INFINITO;
 import android.app.ActivityOptions;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
@@ -19,25 +20,36 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
     private ImageButton btRegister;
     private TextView tvLogin;
     private Button logIn;
     private EditText uName1, uPasswd1;
-    ProgressDialog progressDialog;
+    private static final int RC_SIGN_IN = 100;
+    private SignInButton mGoogleLogIn;
+    GoogleSignInClient mGoogleSignInClient;
+    ProgressDialog progressDialog,progressDialog2;
     private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btRegister  = findViewById(R.id.btRegister);
@@ -45,21 +57,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         logIn =findViewById(R.id.logIn);
         uName1 =findViewById(R.id.uName1);
         uPasswd1 =findViewById(R.id.uPasswd1);
+        mGoogleLogIn =  findViewById(R.id.GoogleBt);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         mAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Logging User In....");
+        progressDialog2 = new ProgressDialog(this);
+        progressDialog2.setMessage("Logging In With Google....");
         btRegister.setOnClickListener(this);
         logIn.setOnClickListener(this);
-
+        mGoogleLogIn.setOnClickListener(this);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View v) {
-        /*if (v==btRegister){*/
-
-        switch (v.getId()) {
+        switch (v.getId())
+        {
             case R.id.btRegister:
                 Intent intent   = new Intent(MainActivity.this,RegisterActivity.class);
                 Pair[] pairs    = new Pair[1];
@@ -73,25 +94,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String Email = uName1.getText().toString().trim();
                 String Password = uPasswd1.getText().toString().trim();
 
-                if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches())
+                {
                     uName1.setError("Enter Valid Email Adress");
                     uName1.setFocusable(true);
                 }
-                else if ((Password.length()<6)){
+                else if ((Password.length()<6))
+                {
 
                     uPasswd1.setError("Password Length atleast 6");
                     uPasswd1.setFocusable(true);
                 }
-                else{
-
+                else {
                     loginUser(Email,Password);
-                }
+                    }
 
                 break;
 
-
+            case R.id.GoogleBt:
+                 signIn();
+                 break;
         }
-
     }
 
 
@@ -101,8 +124,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
 
                             progressDialog.dismiss();
                             FirebaseUser user = mAuth.getCurrentUser();
@@ -110,14 +135,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Intent intent = new Intent(getApplicationContext(), dashboard.class);
                             startActivity(intent);
 
-                        } else {
-
-                            progressDialog.dismiss();
-
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
+                        } else
+                            { progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            }
 
 
                     }
@@ -128,5 +149,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private void signIn()
+    {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-    }}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Toast.makeText(MainActivity.this, "Authentic With Google" , Toast.LENGTH_SHORT).show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed!" +task.getException(), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            Toast.makeText(MainActivity.this, "Name of User : " + personName +"\n" +"User ID : " + personId, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), dashboard.class);
+            startActivity(intent);
+        }
+    }
+}

283396b6143b641061a24ee50123692bf255d568
Manav <manaviitp123@gmail.com>
2019-07-03 16:49:53 +0530

    Login for existing user with email & password is working seamlessly.

--- app/src/main/java/com/mind/INFINITO/MainActivity.java
+++ app/src/main/java/com/mind/INFINITO/MainActivity.java
@@ -1,20 +1,40 @@
 package com.mind.INFINITO;
 
 import android.app.ActivityOptions;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton btRegister;
     private TextView tvLogin;
+    private Button logIn;
+    private EditText uName1, uPasswd1;
+    ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +42,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btRegister  = findViewById(R.id.btRegister);
         tvLogin     = findViewById(R.id.tvLogin);
+        logIn =findViewById(R.id.logIn);
+        uName1 =findViewById(R.id.uName1);
+        uPasswd1 =findViewById(R.id.uPasswd1);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging User In....");
         btRegister.setOnClickListener(this);
+        logIn.setOnClickListener(this);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View v) {
-        if (v==btRegister){
-            Intent intent   = new Intent(MainActivity.this,RegisterActivity.class);
-            Pair[] pairs    = new Pair[1];
-            pairs[0] = new Pair<View,String>(tvLogin,"tvLogin");
-            ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
-            startActivity(intent,activityOptions.toBundle());
+        /*if (v==btRegister){*/
+
+        switch (v.getId()) {
+            case R.id.btRegister:
+                Intent intent   = new Intent(MainActivity.this,RegisterActivity.class);
+                Pair[] pairs    = new Pair[1];
+                pairs[0] = new Pair<View,String>(tvLogin,"tvLogin");
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,activityOptions.toBundle());
+                break;
+
+            case R.id.logIn:
+
+                String Email = uName1.getText().toString().trim();
+                String Password = uPasswd1.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    uName1.setError("Enter Valid Email Adress");
+                    uName1.setFocusable(true);
+                }
+                else if ((Password.length()<6)){
+
+                    uPasswd1.setError("Password Length atleast 6");
+                    uPasswd1.setFocusable(true);
+                }
+                else{
+
+                    loginUser(Email,Password);
+                }
+
+                break;
+
+
         }
+
     }
-}
+
+
+
+    private void loginUser(String email, String password) {
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Registered.." + user.getEmail(), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), dashboard.class);
+                            startActivity(intent);
+
+                        } else {
+
+                            progressDialog.dismiss();
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }}
